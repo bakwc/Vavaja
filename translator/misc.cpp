324d030a356d84a0de738109f5bdc8a648cad201
Filipp Kovalev <kovalev@quantion.ru>
2012-04-28 14:12:59 +0400

    stream #4

---
+++ translator/misc.cpp
@@ -0,0 +1,60 @@
+﻿#include "misc.h"
+
+//------------------------------------------------------------------------------------------
+std::string itoa(int num)	// число в строку
+{
+	std::stringstream out;
+	out << num;
+	return out.str();
+}
+
+//------------------------------------------------------------------------------------------
+int atoi(std::string const &str)  // строку в число
+{
+	std::istringstream stream(str);
+	int number = 0;
+	stream >> number;
+	if (stream.fail()) throw 1;
+	return number;
+}
+
+//------------------------------------------------------------------------------------------
+void error(int line,std::string errMsg)
+{
+    std::cout << "\nERROR! Line " << line << ": " << errMsg << ";\n";
+    exit(19);
+}
+//------------------------------------------------------------------------------------------
+bool isNumber(const std::string &str)
+{
+    unsigned numcnt=0;
+    if (str[0]=='-') numcnt++;
+    for (auto c:str)
+    {
+        if ((c>='0') && (c<='9'))
+        {
+            numcnt++;
+        }
+    }
+    return (str.length()==numcnt);
+}
+//------------------------------------------------------------------------------------------
+bool isFloat(const std::string &str)
+{
+    unsigned numcnt=0;
+    bool isDot=false;
+    if (str[0]=='-') numcnt++;
+    for (auto c:str)
+    {
+        if ((c>='0') && (c<='9'))
+        {
+            numcnt++;
+        }
+        if (c=='.')
+        {
+            numcnt++;
+            isDot=true;
+        }
+    }
+    return ((str.length()==numcnt) && isDot);
+}
---
+++ translator/misc.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+
+std::string itoa(int num);
+int atoi(std::string const &str);
+void error(int line,std::string errMsg);
+bool isNumber(const std::string &str);
+bool isFloat(const std::string &str);
+
